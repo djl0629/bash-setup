646ea3778cfb7a2fdf160c05d51129c4d1016c8e
daijialong <luli@yscredit.com>
2023-05-30 10:22:51 +0800

    节省空间

--- files/docker/docker_install.sh
+++ files/docker/docker_install.sh
@@ -10,8 +10,10 @@ if [ "$arch" = "x86_64" ]; then
 
     # 如果系统没有预装tar则使用unzip解压
     if ! command -v tar &> /dev/null; then
+        rm -rf ./*tgz
         unzip docker-20.10.0-x86_64.zip
     else
+        rm -rf ./*zip
         tar zxvf docker-20.10.0-x86_64.tgz -C /tmp/yscredit/setup/docker
     fi
 
@@ -23,8 +25,10 @@ elif [ "$arch" = "aarch64" ]; then
 
     # 如果系统没有预装tar则使用unzip解压
     if ! command -v tar &> /dev/null; then
+        rm -rf ./*tgz
         unzip docker-20.10.0-aarch64.zip
     else
+        rm -rf ./*zip
         tar zxvf docker-20.10.0-aarch64.tgz -C /tmp/yscredit/setup/docker
     fi
 
